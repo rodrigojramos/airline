eab6a87f3d29b9e49a3b896ccf85c646fc151f9e
Rodrigo Ramos <digojramos@gmail.com>
2024-07-24 10:56:09 -0300

    Passenger

---
+++ src/main/java/com/rodrigoramos/airline/entities/Passenger.java
@@ -0,0 +1,59 @@
+package com.rodrigoramos.airline.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_passenger")
+public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String document;
+
+    public Passenger() {
+    }
+
+    public Passenger(Long id, String name, String email, String document) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.document = document;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+}
